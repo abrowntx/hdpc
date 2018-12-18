c547474c0d9803785a4195451d2ed3c8102df82e
Aaron Brown <Aaron@ezheat.local>
2018-12-18 13:20:04 -0600

    1

--- ver2/template.services.php
+++ ver2/template.services.php
@@ -1,5 +1,19 @@
 <div class="servicesContainer">
-    <a id="services"></a>
-    <div class="topSpace"></div>
-    services
+<a id="services"></a>
+    <div class="servicesWrapper">
+        <div class="servHead">
+            Our Services
+        </div>
+        <div class="serv0"></div>
+        <div class="serv1">
+            <div class="header1">Home PC Repair</div>
+        </div>
+        <div class="serv2">
+            <div class="header1">Business IT Services</div>
+        </div>
+        <div class="serv3">
+            <div class="header1">Cloud Backup</div>
+        </div>
+        <div class="serv4"></div>
+    </div>
 </div>
